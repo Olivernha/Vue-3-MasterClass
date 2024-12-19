bed5ee5e2ae98c007bfa0435483ed1bdf55d2680
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2024-12-19 15:10:08 +0800

    Shadcn UI

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -12,6 +14,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
     },
   },
 })
